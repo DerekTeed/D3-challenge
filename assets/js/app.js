9da41ec2bf9290e0a64898920afce70efc35b782
Derek Teed <Derek@MacBook-Air.local>
2020-07-06 20:54:02 -0500

    update 4

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,46 +27,8 @@ const chartGroup = svg.append("g")
 var chosenXaxis = "age";
 var chosenYaxis = "smokes";
 
-// function used for updating xAxis const upon click on axis label
-function renderXAxes(newXScale, xAxis) {
-  const bottomAxis = d3.axisBottom(newXScale);
-
-  xAxis.transition()
-    .duration(1000)
-    .call(bottomAxis);
-
-  return xAxis;
-}
-// function used for updating yAxis const upon click on axis label
-function renderYAxes(newYScale, yAxis) {
-  const leftAxis = d3.axisLeft(newYScale);
-
-  yAxis.transition()
-    .duration(1000)
-    .call(leftAxis);
-
-  return yAxis;
-}
 
 
-// function used for updating circles group with a transition to
-// new circles
-function renderCircles(circlesGroup, newXScale, newYScale, chosenXAxis, chosenYAxis) {
-
-  circlesGroup.transition()
-    .duration(1000)
-    .attr("cx", d => newXScale(d[chosenXAxis]))
-    .attr("cy", d=>newYScale(d[chosenYAxis]));
-  return circlesGroup;
-}
-function renderTexts(txtGroup, newXScale, newYScale, chosenXAxis, chosenYAxis) {
-
-  txtGroup.transition()
-    .duration(1000)
-    .attr("x", d=>newXScale(d[chosenXAxis]))
-    .attr("y", d=>newYScale(d[chosenYAxis]))
-  return txtGroup;
-}
 
 // function used for updating x-scale const upon click on axis label
 function xScale(healthData, chosenXaxis) {
@@ -78,6 +40,7 @@ function xScale(healthData, chosenXaxis) {
       .range([0, width]);
     return xLinearScale;
 }
+
 function yScale(healthData, chosenYaxis) {
     // create scales
     const yLinearScale = d3.scaleLinear()
@@ -86,43 +49,12 @@ function yScale(healthData, chosenYaxis) {
     return yLinearScale;
 }
 
-// function used for updating tooltip for circles group
-function updateToolTip(chosenXaxis, chosenYaxis, circlesGroup){
-  let xLabel = ""
-  let yLabel = ""
-  xLabel = "Age: ";
-  
-  yLabel = "Smokes: "
-  
-  const toolTip = d3.tip()
-                    .attr("class", "d3-tip")
-                    .offset([80, -60])
-                    .html(function(d){
-                      if (chosenYaxis === "smokes") {
-                        
-                        return(`${d.state},${d.abbr}<br>${xLabel}${d[chosenXaxis]}<br>${yLabel}${d[chosenYaxis]}%`)
-                      }
-            
-                    })
-  
-  circlesGroup.call(toolTip);
-  circlesGroup.on("mouseover", function(data){
-    toolTip.show(data, this);
-    d3.select(this).style("stroke", "black");
-    
-  })
-  circlesGroup.on("mouseout", function(data, index){
-    toolTip.hide(data, this)
-    d3.select(this).style("stroke", "white");
-  })
-  return circlesGroup;
-}
 
 // Retrieve data from the CSV file and execute everything below
 (async function(){
     const healthData = await d3.csv("assets/data/data.csv");
 
-    // parse data to interger from string
+    // parse data to integer from string
     healthData.forEach(function(data){
         
         data.age = +data.age;
@@ -200,92 +132,13 @@ function updateToolTip(chosenXaxis, chosenYaxis, circlesGroup){
                                 .text("Smokes (%)");
                                 
    
-     // updateToolTip function after csv import
-     circlesGroup = updateToolTip(chosenXaxis, chosenYaxis, circlesGroup);
-
-    // x axis labels event listener
-    xlabelsGroup.selectAll("text")
-        .on("click", function() {
-        // get value of selection
-        const value = d3.select(this).attr("value");
-        console.log(`${value} click`)
-        if (value !== chosenXaxis) {
-
-            // replaces chosenXAxis with value
-            chosenXaxis = value;
-            console.log(chosenXaxis)
-
-            // functions here found above csv import
-            // updates x scale for new data
-            xLinearScale = xScale(healthData, chosenXaxis);
-
-            // updates x axis with transition
-            xAxis = renderXAxes(xLinearScale, xAxis);
-
-            // updates circles with new x values
-            circlesGroup = renderCircles(circlesGroup, xLinearScale, yLinearScale, chosenXaxis, chosenYaxis);
-
-             // updates texts with new x values
-            txtGroup = renderTexts(txtGroup, xLinearScale, yLinearScale, chosenXaxis, chosenYaxis);
-
-            // changes classes to change bold text
-             if (chosenXaxis === "age"){
-              
-              ageLabel
-                  .classed("active", true)
-                  .classed("inactive", false);
-              
-            }
-            else{
-             
-                ageLabel
-                    .classed("active", false)
-                    .classed("inactive", true);
-                 
-            }
-          // update tooltip with new info after changing x-axis 
-          circlesGroup = updateToolTip(chosenXaxis, chosenYaxis, circlesGroup); 
-      }})
 // y axis labels event listener
 ylabelsGroup.selectAll("text")
 .on("click", function() {
 // get value of selection
 const value = d3.select(this).attr("value");
 console.log(`${value} click`)
-if (value !== chosenYaxis) {
-
-    // replaces chosenXAxis with value
-    chosenYaxis = value;
-    console.log(chosenYaxis)
-
-    // functions here found above csv import
-    // updates x scale for new data
-    yLinearScale = yScale(healthData, chosenYaxis);
-
-    // updates x axis with transition
-    yAxis = renderYAxes(yLinearScale, yAxis);
-
-    // updates circles with new x values
-    circlesGroup = renderCircles(circlesGroup, xLinearScale, yLinearScale, chosenXaxis, chosenYaxis);
-
-     // updates texts with new x values
-    txtGroup = renderTexts(txtGroup, xLinearScale, yLinearScale, chosenXaxis, chosenYaxis);
-    if (chosenYaxis === "smokes"){
-        
-        smokeLabel
-           .classed("active", true)
-           .classed("inactive", false);
-      
-      }
-    
-     
-        smokeLabel
-          .classed("active", true)
-          .classed("inactive", false);
-      
-    
-     
-     
+if (value == chosenYaxis) {
      // update tooltip with new info after changing y-axis 
      circlesGroup = updateToolTip(chosenXaxis, chosenYaxis, circlesGroup); 
   }})
